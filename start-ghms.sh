8621e50ed75a5e0cec2a9a883dbb982d3d553e85
Z User <z@container>
2026-07-12 12:36:52 +0000

    96d3bde7-0fb1-4359-8201-f640213c8be9

--- start-ghms.sh
+++ start-ghms.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# GHMS Server - persistent with keepalive
 cd /home/z/my-project
-
-while true; do
-  HOSTNAME=0.0.0.0 NODE_OPTIONS="--max-old-space-size=384" \
-    node node_modules/next/dist/bin/next start -p 3000 2>/tmp/next-err.log
-  
-  echo "$(date): Server exited, restarting in 2s..." >> /tmp/ghms.log
-  sleep 2
-done
+export HOSTNAME=0.0.0.0
+export NODE_OPTIONS="--max-old-space-size=384"
+exec node node_modules/next/dist/bin/next start -p 3000
