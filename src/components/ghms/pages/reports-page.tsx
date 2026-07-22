f5f42a542ede1dc42931fbf9c0fc80dda2850ecb
Z User <z@container>
2026-07-22 20:06:28 +0000

    fix: remove blank line between )} and Summary Cards comment

--- src/components/ghms/pages/reports-page.tsx
+++ src/components/ghms/pages/reports-page.tsx
@@ -545,7 +545,6 @@ export default function ReportsPage() {
               </CardContent>
             </Card>
           )}
-
           {/* Summary Cards */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             <Card>
